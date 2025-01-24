f714f18fc2ce322d0bf2572e189e13a948267715
vaibhavkothari33 <vaibhavkothari50@gmail.com>
2025-01-24 14:58:54 +0530

    little update

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { Outlet } from 'react-router-dom'
 import Navbar from './components/Navbar/Navbar'
+import ScrollToTop from './components/ScrollToTop'
 // import Navbar from './Navbar'
 function App() {
   return (
     <>
+    <ScrollToTop/>
     <Navbar/>
     <Outlet/>
     </>
--- src/main.jsx
+++ src/main.jsx
@@ -1,31 +1,28 @@
-import ReactDOM from 'react-dom/client'
-import React from 'react'
-import { createRoutesFromElements, Route, RouterProvider} from 'react-router-dom'
-import { createBrowserRouter } from 'react-router-dom'
-import App from './App'
-import HeroSection from './components/HeroSection/HeroSection'
-import './index.css'
-import GalleryGrid from './components/GalleryGrid/GalleryGrid'
-import TeamSection from './components/Team/TeamSection'
-import Events from './components/Events/Events'
-import News from './components/News/News'
-
+import ReactDOM from 'react-dom/client';
+import React from 'react';
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom';
+import App from './App';
+import HeroSection from './components/HeroSection/HeroSection';
+import GalleryGrid from './components/GalleryGrid/GalleryGrid';
+import TeamSection from './components/Team/TeamSection';
+import Events from './components/Events/Events';
+import News from './components/News/News';
+import './index.css';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path='/' element={<App/>}>
-      <Route path ='' element={<HeroSection/>}/>
-      <Route path ='team' element={<TeamSection/>}/>
-      <Route path ='events' element={<Events/>}/>
-      <Route path ='gallery' element={<GalleryGrid/>}/>
-      <Route path ='news' element={<News/>}/>
-
+    <Route path="/" element={<App />}>
+      <Route index element={<HeroSection />} />
+      <Route path="team" element={<TeamSection />} />
+      <Route path="events" element={<Events />} />
+      <Route path="gallery" element={<GalleryGrid />} />
+      <Route path="news" element={<News />} />
     </Route>
   )
-)
+);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router}/>
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
