c79589b8877ff4a1c5d33a623b66d60478b712d1
vaibhavkothari33 <vaibhavkothari50@gmail.com>
2025-01-31 13:19:04 +0530

    photos

--- src/components/GalleryGrid/GalleryGrid.jsx
+++ src/components/GalleryGrid/GalleryGrid.jsx
@@ -13,9 +13,16 @@ const GalleryGrid = () => {
     { id: 4, src: "/cool2.png", alt: "Event 3" },
     { id: 5, src: "/image.png", alt: "Vaibhav 2" },
     { id: 6, src: "/cool1.png", alt: "Event 6" },
+    
     { id: 10, src: "/cool3.png", alt: "Event 7" },
     { id: 11, src: "https://i.ibb.co/ymdCqj4q/Screenshot-2025-01-31-030711.png", alt: "Event 9" },
     { id: 12, src: "https://i.ibb.co/rP71Ncb/Screenshot-2025-01-31-030701.png", alt: "Event 10" },
+    
+    { id: 12, src: "https://i.ibb.co/99vwdZ9N/Whats-App-Image-2025-01-31-at-3-16-37-AM.jpg", alt: "Event 10" },
+    { id: 12, src: "https://i.ibb.co/G4FT1CzV/Whats-App-Image-2025-01-31-at-3-16-36-AM-1.jpg", alt: "Event 10" },
+    { id: 12, src: "https://i.ibb.co/j9pTzz0R/Whats-App-Image-2025-01-31-at-3-16-36-AM.jpg", },
+  
+    { id: 12, src: "https://i.ibb.co/v6ZXCnSW/Whats-App-Image-2025-01-31-at-3-16-37-AM-1.jpg", alt: "Event 10" },
   ];
 
   return (
